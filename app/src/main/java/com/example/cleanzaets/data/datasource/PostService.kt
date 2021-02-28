aac64cc3617f397e13bdbaf9d2556a923adf1259
Oleh Zaiets <59374316+ZGoblin@users.noreply.github.com>
2021-02-28 15:22:05 +0200

    impl retrofit2

--- app/src/main/java/com/example/cleanzaets/data/datasource/PostService.kt
+++ app/src/main/java/com/example/cleanzaets/data/datasource/PostService.kt
@@ -1,10 +1,10 @@
 package com.example.cleanzaets.data.datasource
 
 import com.example.cleanzaets.data.datasource.database.Post
-import retrofit2.Call
+import io.reactivex.Observable
 import retrofit2.http.GET
 
 interface PostService {
     @GET("/posts")
-    fun getPosts(): Call<List<Post>>
+    fun getPosts(): Observable<List<Post>>
 }
